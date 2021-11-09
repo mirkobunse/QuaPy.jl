972d07e016620da3bc78361e0450d06aa09fa8f4
Documenter.jl <documenter@juliadocs.github.io>
2021-11-09 16:16:12 +0000

    build based on 6ffb666

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QuaPy","category":"page"},{"location":"#QuaPy","page":"Home","title":"QuaPy","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuaPy.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QuaPy]","category":"page"}]
+}
