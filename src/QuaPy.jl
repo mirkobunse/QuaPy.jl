c6afe68a81e6b6b101b90cd671dd6c155be7585c
Mirko Bunse <mirko.bunse@cs.tu-dortmund.de>
2021-11-09 11:11:50 +0100

    Import and install the Python package QuaPy

--- src/QuaPy.jl
+++ src/QuaPy.jl
@@ -1,5 +1,17 @@
 module QuaPy
 
-# Write your package code here.
+using Conda, PyCall
+
+# import the Python package "quapy" into __QUAPY
+const __QUAPY = PyNULL()
+function __init__()
+    quapy = pyimport_e("quapy")
+    if ispynull(quapy) # need to install quapy?
+        Conda.pip_interop(true)
+        Conda.pip("install", "quapy")
+        quapy = pyimport("quapy")
+    end
+    copy!(__QUAPY, quapy)
+end
 
 end
